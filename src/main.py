0ea5b63316e9a0c09fe67b3077a527a67b185b88
DeluxeOwl <nospamplease0@protonmail.com>
2020-04-21 18:22:33 +0300

    Added main page

--- src/main.py
+++ src/main.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
-
+from PyQt5 import QtWidgets, uic
 
 import sys
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QPushButton
 
+app = QtWidgets.QApplication([])
 
-def main():
-    """Simple Hello World example with PyQt5."""
+# specify the location of your .ui file
+win = uic.loadUi("../user_interface/mainPage.ui")
 
-    # 1. Import `QApplication` and all the required widgets
-    # 2. Create an instance of QApplication
-    app = QApplication(sys.argv)
+win.show()
 
-    # 3. Create an instance of your application's GUI
-    window = QWidget()
-    window.setWindowTitle('PyQt5 App')
-    window.setGeometry(450, 450, 300, 300)
-    window.move(60, 15)
-
-    hello_msg = QLabel('<h1>Hello World!</h1>', parent=window)
-    hello_msg.move(60, 15)
-
-    register_button = QPushButton('Register', parent=window)
-
-    # 4. Show your application's GUI
-    window.show()
-
-    # 5. Run your application's event loop (or main loop)
-    sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+sys.exit(app.exec())
