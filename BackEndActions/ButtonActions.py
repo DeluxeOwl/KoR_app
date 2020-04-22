d45a20814c5ea2a9ca982f27712df5db50a2d88a
DeluxeOwl <nospamplease0@protonmail.com>
2020-04-22 15:43:45 +0300

    Modified MyWindow class and added sqlite3

--- BackEndActions/ButtonActions.py
+++ BackEndActions/ButtonActions.py
@@ -1,9 +1,10 @@
 import sqlite3
+import os
 
 
 def loginButtonClicked(ceva=''):
     print(f"Login button clicked {ceva}")
 
 
-def registerButtonClicked(username, password):
-    pass
+def registerButtonClicked(username='', password=''):
+    print(f'{username} {password}')
--- main.py
+++ main.py
@@ -5,17 +5,34 @@ from UserInterface.mainPage import Ui_MainWindow
 from BackEndActions.ButtonActions import *
 
 import sys
+import os
+import sqlite3
 
 
 class MyWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(MyWindow, self).__init__()
+        self.initializeVariables()
+        self.databaseConnection()
+        self.buttonClickActions()
+
+    def initializeVariables(self):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.database = os.path.dirname(
+            os.path.abspath(__file__))+'/BackEndActions/users.db'
+
+    def databaseConnection(self):
+        self.conn = sqlite3.connect(self.database)
+        self.conn.close()
+
+    def buttonClickActions(self):
         # Use lambda to be able to use arguments
         self.ui.loginButton.clicked.connect(
             lambda: loginButtonClicked("something"))
+        self.ui.registerButton.clicked.connect(
+            lambda: registerButtonClicked('JohnDoe', 'password'))
 
 
 try:
