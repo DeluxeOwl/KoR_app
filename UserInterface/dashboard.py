3805e63b3a7d877fda0a333a555260865470af51
DeluxeOwl <nospamplease0@protonmail.com>
2020-05-01 17:51:28 +0300

    Added directory view

--- UserInterface/dashboard.py
+++ UserInterface/dashboard.py
@@ -24,39 +24,57 @@ class Ui_LoggedWindow(object):
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        spacerItem = QtWidgets.QSpacerItem(
-            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
-        self.treeView = QtWidgets.QTreeView(self.centralwidget)
-        self.treeView.setObjectName("treeView")
-        self.gridLayout.addWidget(self.treeView, 1, 0, 1, 5)
+        self.pushButtonChangePassword = QtWidgets.QPushButton(
+            self.centralwidget)
+        self.pushButtonChangePassword.setObjectName("pushButtonChangePassword")
+        self.gridLayout.addWidget(self.pushButtonChangePassword, 0, 4, 1, 1)
         self.pushButtonLogout = QtWidgets.QPushButton(self.centralwidget)
         self.pushButtonLogout.setObjectName("pushButtonLogout")
-        self.gridLayout.addWidget(self.pushButtonLogout, 0, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButtonLogout, 0, 5, 1, 1)
+
+        self.treeView = QtWidgets.QTreeView(self.centralwidget)
+        self.treeView.setObjectName("treeView")
+
+        self.gridLayout.addWidget(self.treeView, 1, 0, 1, 6)
+        self.pushButtonGroups = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButtonGroups.setObjectName("pushButtonGroups")
+        self.gridLayout.addWidget(self.pushButtonGroups, 0, 3, 1, 1)
         self.labelUsername = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
         font.setPointSize(24)
         self.labelUsername.setFont(font)
         self.labelUsername.setObjectName("labelUsername")
         self.gridLayout.addWidget(self.labelUsername, 0, 0, 1, 1)
-        self.pushButtonChangePassword = QtWidgets.QPushButton(
-            self.centralwidget)
-        self.pushButtonChangePassword.setObjectName("pushButtonChangePassword")
-        self.gridLayout.addWidget(self.pushButtonChangePassword, 0, 3, 1, 1)
-        self.pushButtonGroups = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonGroups.setObjectName("pushButtonGroups")
-        self.gridLayout.addWidget(self.pushButtonGroups, 0, 2, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
+        self.pushButtonOpenFiles = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButtonOpenFiles.setObjectName("pushButtonOpenFiles")
+        self.gridLayout.addWidget(self.pushButtonOpenFiles, 0, 2, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         LoggedWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(LoggedWindow)
         QtCore.QMetaObject.connectSlotsByName(LoggedWindow)
 
+        self.setDirectory()
+
     def retranslateUi(self, LoggedWindow):
         _translate = QtCore.QCoreApplication.translate
         LoggedWindow.setWindowTitle(_translate("LoggedWindow", "Dashboard"))
-        self.pushButtonLogout.setText(_translate("LoggedWindow", "Log out"))
-        self.labelUsername.setText(_translate("LoggedWindow", "Username"))
         self.pushButtonChangePassword.setText(
             _translate("LoggedWindow", "Change Password"))
+        self.pushButtonLogout.setText(_translate("LoggedWindow", "Log out"))
         self.pushButtonGroups.setText(_translate("LoggedWindow", "Groups"))
+        self.labelUsername.setText(_translate("LoggedWindow", "Username"))
+        self.pushButtonOpenFiles.setText(
+            _translate("LoggedWindow", "Open Files"))
+
+    def setDirectory(self, dir=''):
+        # Handle filesystem
+        self.model = QtWidgets.QFileSystemModel()
+        self.model.setRootPath(QtCore.QDir.rootPath()
+                               )              # Get root path
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(self.model.index(
+            "/home"+dir))  # Choose directory to display
